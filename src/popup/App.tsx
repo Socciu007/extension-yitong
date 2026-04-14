3b04e12ce480df597261ff67558e750d524aca59
Money <manhtien310701@gmail.com>
2026-04-14 11:00:51 +0800

    add fill-yitong

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -19,102 +19,160 @@ export default function App() {
       active: true,
       currentWindow: true,
     })
-    const url = "https://www.eptrade.cn/epb/login/scno_direct_bk.html"
+    const url1 = "https://www.eptrade.cn/epb/login/scno_direct_bk.html"
+    const url = "https://www.eptrade.cn/epb/index.jsp"
 
     // Load URL
-    // await chrome.tabs.update(tab.id, { url })
+    await chrome.tabs.update(tab.id, { url })
     // Wait for tab to load
     // await loadTab(tab)
+    // Check url 
+    const currentUrl = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    })
+    if (currentUrl[0]?.url?.includes('login')) {
+      await chrome.tabs.update(tab.id, { url: url1 })
+      await loadTab(tab)
+      // Action Login
+      const urlImg = await chrome.scripting.executeScript({
+        target: { tabId: tab.id as number },
+        func: async () => {
+          try {
+            console.log("Start action login....")
+            const tabLogin = document.querySelectorAll(".tabs-title") as NodeListOf<Element>
+            if (tabLogin) (tabLogin[1] as HTMLAnchorElement).click()
+            
+            const username = document.querySelector("#user_id") as HTMLInputElement
+            if (username) username.value = "CN122887"
+            const password = document.querySelector("#user_pwd" ) as HTMLInputElement
+            if (password) password.value = "imND3I26"
+            const capchaCodeImg = document.querySelector("#safecode") as HTMLButtonElement
+            if (capchaCodeImg) {
+              const urlImg = capchaCodeImg.getAttribute("src")
+              return urlImg
+            }
+          } catch (error) {
+            console.error("Error login in the page", error)
+          }
+        },
+      })
+      await delay(1000)
+  
+      // Decode capcha code
+      const toBase64 = await axios.get(`https://www.eptrade.cn/epb/login/${urlImg[0].result}`, { responseType: "arraybuffer" })
+      const capchaCode = await decodeCapcha(
+        Buffer.from(toBase64.data, "binary").toString("base64")
+      )
+      if (!capchaCode) {
+        showToast("Please login to the system!", "warning")
+        return
+      }
 
-    // Action Login
-    // const urlImg = await chrome.scripting.executeScript({
-    //   target: { tabId: tab.id as number },
-    //   func: async () => {
-    //     try {
-    //       console.log("Start action login....")
-    //       const tabLogin = document.querySelectorAll(".tabs-title") as NodeListOf<Element>
-    //       if (tabLogin) (tabLogin[1] as HTMLAnchorElement).click()
-          
-    //       const username = document.querySelector("#user_id") as HTMLInputElement
-    //       if (username) username.value = "CN122887"
-    //       const password = document.querySelector("#user_pwd" ) as HTMLInputElement
-    //       if (password) password.value = "imND3I26"
-    //       const capchaCodeImg = document.querySelector("#safecode") as HTMLButtonElement
-    //       if (capchaCodeImg) {
-    //         const urlImg = capchaCodeImg.getAttribute("src")
-    //         return urlImg
-    //       }
-    //       // const loginBtn = document.querySelector("#btnLogin") as HTMLButtonElement
-    //       // if (loginBtn) loginBtn.click()
-    //     } catch (error) {
-    //       console.error("Error login in the page", error)
-    //     }
-    //   },
-    // })
-    // await delay(1000)
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id as number },
+        func: async (capchaCode: string) => {
+          const capchaCodeInput = document.querySelector("#login2 > table > tbody > tr:nth-child(3) > td:nth-child(1) > input") as HTMLInputElement
+          if (capchaCodeInput) capchaCodeInput.value = capchaCode
 
-    // const toBase64 = await axios.get(`https://www.eptrade.cn/epb/login/${urlImg}`, { responseType: "arraybuffer" })
-    // console.log(
-    //   "toBase64",
-    //   Buffer.from(toBase64.data, "binary").toString()
-    // )
-    // const capchaCode = await decodeCapcha(
-    //   Buffer.from(toBase64.data, "binary").toString("base64")
-    // )
-    // console.log("capchaCode", capchaCode)
+          const loginBtn = document.querySelector("#btnLogin") as HTMLButtonElement
+          if (loginBtn) loginBtn.click()
+        },
+        args: [capchaCode],
+      })
+    }
+
     await delay(1000)
     await chrome.scripting.executeScript({
       target: { tabId: tab.id as number },
-      func: async (blNo, truck) => {
+      func: async (blNo: string, truck: string) => {
+        const waitFor = <T extends Element>(
+          selector: string,
+          root: Document | Element = document,
+          timeout: number = 10000
+        ): Promise<T> => {
+          return new Promise((resolve, reject) => {
+            const start = Date.now();
+            const timer = setInterval(() => {
+              const el = root.querySelector(selector) as T;
+              if (el) {
+                clearInterval(timer);
+                resolve(el);
+              } else if (Date.now() - start > timeout) {
+                clearInterval(timer);
+                reject(new Error(`Timeout: ${selector}`));
+              }
+            }, 200);
+          });
+        }
         const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
         try {
-          const tabInfo = document.querySelectorAll('a[target="mainFrame"]')[1] as HTMLAnchorElement
-          if (tabInfo) tabInfo.click()
-          await delay(2000)
-          
-          const iframe = document.querySelectorAll("body iframe")[1] as HTMLIFrameElement
-          if (iframe) {
-            const blNoInput = iframe.contentDocument?.querySelector('input[name="param.like.ebw2Booking.bookingNo"]') as HTMLInputElement
-            if (blNoInput) {
-              blNoInput.value = blNo.includes('ONEY') ? blNo.replace('ONEY', '') : blNo
-              blNoInput.dispatchEvent(new Event("input", { bubbles: true }))
-              blNoInput.dispatchEvent(new Event("change", { bubbles: true }))
-            }
-            
-            const searchBtn = iframe.contentDocument?.querySelector("[onclick=\"query('cdusform','book_list')\"]") as HTMLButtonElement
-            if (searchBtn) searchBtn.click()
-            await delay(2000)
-            
-            const chooseBtn = iframe.contentDocument?.querySelector(
-              "body > div.text > div > div.datagrid-wrap.panel-body > div.datagrid-view > div.datagrid-view2 > div.datagrid-body > table > tbody > tr > td:nth-child(1) > div > input[type=checkbox]"
-            ) as HTMLButtonElement
-            if (chooseBtn) chooseBtn.click()
-            
-            const editBtn =  iframe.contentDocument?.querySelector(
-              "body > div.text > div > div.datagrid-wrap.panel-body > div.datagrid-toolbar > a:nth-child(9) > span > span"
-            ) as HTMLButtonElement
-            if (editBtn) editBtn.click()
-            
-            const yn = iframe.contentDocument?.querySelector("#chooseCa") as HTMLInputElement
-            if (yn) yn.click()
-            
-            const truckInput = iframe.contentDocument?.querySelector("div:nth-child(1) > span > input.combo-text.validatebox-text") as HTMLInputElement
-            if (truckInput) truckInput.value = truck.trim() as string
-            if (truckInput) truckInput.dispatchEvent(new Event("input", { bubbles: true }))
-            if (truckInput) truckInput.dispatchEvent(new Event("change", { bubbles: true }))
-            const searchTruck = iframe.contentDocument?.querySelector(
-              'input[onclick="queryCarrier()"]'
-            ) as HTMLInputElement
-            if (searchTruck) searchTruck.click()
-          }
-        } catch (error) {
-          console.error("Error click on the tab info", error)
+          // 1. click tab
+          console.clear()
+          console.log("Start click tab...")
+          const tabInfo = await waitFor<HTMLAnchorElement>(
+            'div > div.panel-body.accordion-body > ul > li:nth-child(2) > div > a[target="mainFrame"]'
+          )
+          tabInfo.click()
+          await delay(1500)
+
+          // 2. Get the iframe
+          const iframe = await waitFor<HTMLIFrameElement>("#tabs > div.tabs-panels.tabs-panels-noborder > div:nth-child(2) > div > iframe")
+          const doc = iframe.contentDocument as Document | null
+
+          if (!doc) throw new Error("iframe not loaded");
+
+          // 3. input BL
+          const blInput = await waitFor<HTMLInputElement>('input[name="param.like.ebw2Booking.bookingNo"]', doc)
+
+          blInput.value = blNo.includes("ONEY")
+            ? blNo.replace("ONEY", "")
+            : blNo
+          blInput.dispatchEvent(new Event("input", { bubbles: true }))
+          blInput.dispatchEvent(new Event("change", { bubbles: true }))
+
+          // 4. search
+          const searchBtn = await waitFor<HTMLInputElement>('input[onclick="query(\'cdusform\',\'book_list\')"]', doc)
+          searchBtn.click()
+          await delay(4000)
+
+          // 5. checkbox
+          const checkbox = await waitFor<HTMLInputElement>(
+            "body > div.text > div > div.datagrid-wrap.panel-body > div.datagrid-view > div.datagrid-view2 > div.datagrid-body > table > tbody > tr > td:nth-child(1) input",
+            doc
+          )
+          checkbox.click()
+
+          // 6. edit
+          const editBtn = await waitFor<HTMLElement>(".datagrid-toolbar a:nth-child(9)", doc)
+          editBtn.click()
+
+          // 7. choose CA
+          const yn = await waitFor<HTMLInputElement>("#chooseCa", doc)
+          yn.click()
+
+          // 8. input truck
+          const truckInput = await waitFor<HTMLInputElement>("#zdca > div > div > div > div:nth-child(1) > span > input.combo-text.validatebox-text", doc)
+          truckInput.value = truck.trim()
+          truckInput.dispatchEvent(new Event("input", { bubbles: true }))
+          truckInput.dispatchEvent(new Event("change", { bubbles: true }))
+          await delay(1000)
+
+          // 9. search truck
+          const searchTruck = await waitFor<HTMLInputElement>('input[onclick="queryCarrier()"]', doc)
+          searchTruck.click()
+          await delay(3000)
+
+          return true
+        } catch (err) {
+          console.error("ERROR:", err);
+          return false;
         }
       },
-      args: [blNo, truck]
+      args: [blNo, truck],
     })
 
-    await delay(3000)
     await chrome.scripting.executeScript({
       target: { tabId: tab.id as number },
       func: async () => {
@@ -123,7 +181,7 @@ export default function App() {
             "body iframe"
           )[1] as HTMLIFrameElement
           const chooseTruckBtn = iframe.contentDocument?.querySelector(
-            "div.datagrid-body > table > tbody > tr:nth-child(2) > td:nth-child(1) > div > input[type=checkbox]"
+            "#zdca > div > div > div > div.panel.datagrid > div > div.datagrid-view > div.datagrid-view2 > div.datagrid-body > table > tbody > tr:nth-child(2) > td:nth-child(1) > div > input[type=checkbox]"
           ) as HTMLButtonElement
           if (chooseTruckBtn) chooseTruckBtn.click()
 
